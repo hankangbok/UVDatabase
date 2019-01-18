9cac354612e9c0fac59d7f9e5b7697e1eb7a574f
Kang <HKang@calacademy.org>
2019-01-17 17:15:06 -0800

    outputs text that can be saved as a JS object

--- scripts/MasterDictToCSV.py
+++ scripts/MasterDictToCSV.py
@@ -9,7 +9,7 @@ def createJSFile():
     JSResult.write("This is a test")
 #create a csv file (need to check if this works, since I've had issues with bcb permissions
 def createCSVFile(dictionary):
-    CSVResult = open("DTSource.CSV", "w+")
+    CSVResult = open("DTSource.CSV", "w")
     CSVResult.write("This is a test")
     CSVResult.write("asset folder,module name,source,profiles\n")
     for item in dictionary:
@@ -25,4 +25,53 @@ def createCSVFile(dictionary):
 
 dictionary = findModuleFolders()
 #createJSFile()
-createCSVFile(dictionary)
+#createCSVFile(dictionary)
+#Okay, it really won't let me write to the csv or js files
+#lets see if I can make a printed output that works if I paste it into the js file.
+def createCSVLines(dictionary):
+    finalText = "asset folder,module name,source,profiles\n"
+    for item in dictionary:
+        currentLine = str(item)+",,,"+str(dictionary[item])+"\n"
+        finalText+=currentLine
+    print finalText
+    return finalText
+def convertCSVtoJSFormat(dictionary):
+   finalText=''
+   finalText+="var dataSet = ["
+   for item in dictionary:
+       currentLine = str(item)+",,,"+str(dictionary[item])+"\n"
+       finalText+="["+currentLine+"],\n"
+   finalText=finalText[:-2]
+   finalText+="];\n"
+   finalText+="""$(document).ready(function() {
+			$('#example').DataTable( {
+				mark:true,
+				autoWidth:false,
+				data: dataSet,
+				"pageLength":30,
+				columns: [
+					{ title: "Asset Folder"},
+					{ title: "Module Name" },
+					{ title: "Date of Creation" },
+					{ title: "Profiles"}
+
+				],
+				           columnDefs: [
+                {
+                    render: function (data, type, full, meta) {
+                        return "<div class='text-wrap width-200'>" + data + "</div>";
+                    },
+                    targets: 3
+                }
+             ]
+			} );
+			$.extend(true, $.fn.dataTable.defaults, {
+				mark: true
+			});
+			$('div.dataTables_filter input').focus();
+
+		});"""
+   print finalText
+
+#createCSVLines(dictionary)
+convertCSVtoJSFormat(dictionary)
